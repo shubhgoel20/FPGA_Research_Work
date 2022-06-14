a43dceec73d97557ca4ef2f80359c9ade9d547ef
shubh <shubh20goel@gmail.com>
2022-06-14 13:38:41 +0530

    Added some changes

--- node.v
+++ node.v
@@ -9,9 +9,9 @@ always @(posedge clk) begin
 end
 endmodule
 
-module node(in,in1,in2,in3,in4,clk,out);
+module node(in,in1,in2,in3,in4,clk,high,out);
 input [1:0] in,in1,in2,in3,in4;
-input clk;
+input clk,high;
 output reg out;
 
 reg [1:0] temp;
@@ -21,39 +21,46 @@ reg rst;
 wire y;
 
 initial begin
-temp = 2'b11;
-out = 0;
-temp_o = 0;
-rst = 0;
+    temp = 2'b11;
+    out = 0;
+    temp_o = 0;
+    rst = 0;
 end
 
 twobit_comparator cmp (in,temp,y);
-counter c(clk,rst,cnt);
+counter c(clk,rst,cnt); //counter will work as selector line for the mux
 
 
 always @ (posedge clk)
 begin
-out=0;
-rst = 1;
-case(cnt)
-    3'b000:begin
-        temp = in1;
-        temp_o=(temp_o|y);
-        end
-    3'b001:begin
-        temp = in2;
-        temp_o=(temp_o|y);
-        end
-    3'b010:begin
-        temp = in3;
-        temp_o=(temp_o|y);
-        rst=0;
-        end
-    3'b011:begin
-        temp = in4;
-        out=(temp_o|y);
-        temp_o=0;
-        end
-endcase
+if(high == 0) begin 
+    out = 0;
+    rst = 0;
+end
+else begin
+    out=0;
+    rst = 1;
+    case(cnt)
+        3'b000:begin
+            temp = in1;
+            temp_o=(temp_o|y);
+            end
+        3'b001:begin
+            temp = in2;
+            temp_o=(temp_o|y);
+            end
+        3'b010:begin
+            temp = in3;
+            temp_o=(temp_o|y);
+            rst=0;
+            end
+        3'b011:begin
+            temp = in4;
+            out=(temp_o|y);
+            temp_o=0;
+            end
+    endcase
+    
+end
 end
 endmodule
--- node_tb.v
+++ node_tb.v
@@ -5,16 +5,18 @@ module node_tb;
 	// Inputs
 	reg [1:0] in,in1,in2,in3,in4;
     reg clk;
+    reg high;
 	// Outputs
 	wire out;
 
     initial begin
         clk = 0;
+        high = 1;
     end
 
 	// Instantiate the Unit Under Test (UUT)
 	node uut(
-		.in(in), .in1(in1), .in2(in2), .in3(in3), .in4(in4),.clk(clk),
+		.in(in), .in1(in1), .in2(in2), .in3(in3), .in4(in4),.clk(clk),.high(high),
 		.out(out)
 	);
 
@@ -27,6 +29,7 @@ module node_tb;
     reg [9:0] i = 0;
     
     always @(posedge clk) begin 
+        high = 1;
         {in,in1,in2,in3,in4} = i;
         #40;
         i = i+1;
