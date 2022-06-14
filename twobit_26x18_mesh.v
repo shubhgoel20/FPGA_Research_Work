d55cde95fb353bc3b7e77e84a7a820585c1583c2
shubh <shubh20goel@gmail.com>
2022-06-14 13:34:47 +0530

    Updated the Code

--- twobit_26x18_mesh.v
+++ twobit_26x18_mesh.v
@@ -1,15 +1,25 @@
 `timescale 1ns / 1ps
 module twobit_26x18_mesh(
-input [935:0] inp,
+input [51:0] inp,      // 26 inputs, 2 bit each, concatenated
+input [4:0] row,       // for inputting the values at the right place
 input clk,
+input high, //when high = 1, all the inputs have been scaned. Output will then be generated after 4 clock cycles.
 output [467:0] out
 );
 
+integer t;
+//Input section
+reg [1:0] in[0:467];
+always @ (*) begin
+    for ( t = 0 ; t<26 ; t=t+1 ) begin
+	    in[26*row+t] = {inp[2*t],inp[(2*t)+1]};
+    end
+end
 
 genvar j;
 generate
-	for ( j = 467 ; j>=0 ; j=j-1 ) begin: comparator_block
-	    node wirexy ({inp[(2*j)+1],inp[2*j]},{inp[(2*((j-26+468)%468))+1],inp[2*((j-26+468)%468)]},{inp[(2*((j-j%26)+(j-1+26)%26))+1],inp[2*((j-j%26)+(j-1+26)%26)]},{inp[(2*((j-j%26)+(j+1+26)%26))+1],inp[2*((j-j%26)+(j+1+26)%26)]},{inp[(2*((j+26+468)%468))+1],inp[2*((j+26+468)%468)]},clk,out[j]);
+	for ( j = 0 ; j<468 ; j=j+1 ) begin: comparator_block
+		node wirexy (in[j],in[(j-26+468)%468],in[(j-j%26)+(j-1+26)%26],in[(j-j%26)+(j+1+26)%26],in[(j+26+468)%468],clk,high,out[467-j]);
 	end
 endgenerate
 
--- twobit_26x18_mesh_tb.v
+++ twobit_26x18_mesh_tb.v
@@ -1,15 +1,20 @@
 `timescale 1ns / 1ps
 
 module twobit_26x18_mesh_tb;
-	reg [935:0] inp;
+	reg [51:0] inp;
+	reg [4:0] row;
 	reg clk;
+	reg high;
 	integer i;
     wire [467:0] out;
+	reg [1:0] in[0:5];
 
 	// Instantiate the Unit Under Test (UUT)
 	twobit_26x18_mesh uut (
 		.inp(inp),
+		.row(row),
 		.clk(clk),
+		.high(high),
         .out(out)
 	);
 
@@ -19,12 +24,33 @@ module twobit_26x18_mesh_tb;
             #50 clk = ~clk;
         end
     end
+    
+    initial begin 
+        in[0] = 2'b01;
+		in[1] = 2'b00;
+		in[2] = 2'b10;
+		in[3] = 2'b10;
+		in[4] = 2'b01;
+		in[5] = 2'b11;
+    end
 
 	always @(posedge clk) begin
-		for ( i=0 ; i<936 ; i=i+1 ) begin
-            inp[i] = ($random)%2;
+        high = 0;
+        for ( i=0 ; i<18 ; i=i+1 ) begin
+            #5;row = i;
+            inp = {2'b11,in[i%6],2'b11,in[i%6],2'b11,in[i%6],2'b11,in[i%6],in[i%6],in[i%6],in[i%6],in[i%6],in[i%6],in[i%6],in[i%6],in[i%6],in[i%6],in[i%6],in[i%6],in[i%6],in[i%6],in[i%6],in[i%6],in[i%6],in[i%6],in[i%6]};
         end
-        #400;
+        #10;
+        //Inputs will take one clock cycle to stablize.
+        high = 1;
+        #500;
+        //Output will become after 4 clock cycles and will remain active for one clock cycle.
+        in[0] = {($random)%2,($random)%2};
+		in[1] = {($random)%2,($random)%2};
+		in[2] = {($random)%2,($random)%2};
+		in[3] = {($random)%2,($random)%2};
+		in[4] = {($random)%2,($random)%2};
+		in[5] = {($random)%2,($random)%2};
 	end
   
 endmodule
