d7cbb1ddf4487eb70501a26584e5e17ab4941da6
shubh <shubh20goel@gmail.com>
2022-06-20 14:13:00 +0530

    Fixed error

--- pixel_following.cpp
+++ pixel_following.cpp
@@ -19,21 +19,12 @@ int main(){
     for(int i = 0;i<n;i++){
         for(int j = 0;j<m;j++){
             if(!img[i][j]){
-                if(j+1<m){
-                    if(img[i][j+1]) contour[i][j+1] = 1;
-                }
-                if(i+1<n){
-                    if(img[i+1][j]) contour[i+1][j] = 1;
-                }
+                if(img[i][(j+1)%m]) contour[i][(j+1)%m] = 1;
+                if(img[(i+1)%n][j]) contour[(i+1)%n][j] = 1;
             }
             else{
                 if(!contour[i][j]){
-                    if(j+1<m){
-                        if(!img[i][j+1]) contour[i][j] = 1;
-                    }
-                    if(i+1<n){
-                        if(!img[i+1][j]) contour[i][j] = 1;
-                    }
+                    if(!img[i][(j+1)%m] || !img[(i+1)%n][j]) contour[i][j] = 1;
                 }
             }
         }
