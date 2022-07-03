f276c02a808494d1d2fc5a1a697c9b39d9923be8
shubh <shubh20goel@gmail.com>
2022-07-03 16:27:15 +0530

    Updated code

--- node.v
+++ node.v
@@ -12,55 +12,53 @@ endmodule
 module node(in,in1,in2,in3,in4,clk,high,out);
 input [1:0] in,in1,in2,in3,in4;
 input clk,high;
-output reg out;
+output reg [3:0] out;
 
 reg [1:0] temp;
-reg temp_o;
+reg [3:0] temp_o;
 wire [2:0] cnt;
 reg rst;
 wire y;
 
 initial begin
-    temp = 2'b11;
-    out = 0;
-    temp_o = 0;
-    rst = 0;
+temp_o = 4'b0000;
+temp = 2'b11;
+out = 4'b0000;
+rst = 0;
 end
 
 twobit_comparator cmp (in,temp,y);
-counter c(clk,rst,cnt); //counter will work as selector line for the mux
+counter c(clk,rst,cnt);
 
-
-always @ (posedge clk)
+always @ (*)
 begin
-if(high == 0) begin 
-    out = 0;
-    rst = 0;
-end
-else begin
-    out=0;
-    rst = 1;
-    case(cnt)
-        3'b000:begin
-            temp = in1;
-            temp_o=(temp_o|y);
-            end
-        3'b001:begin
-            temp = in2;
-            temp_o=(temp_o|y);
-            end
-        3'b010:begin
-            temp = in3;
-            temp_o=(temp_o|y);
-            rst=0;
-            end
-        3'b011:begin
-            temp = in4;
-            out=(temp_o|y);
-            temp_o=0;
-            end
-    endcase
-    
-end
+    if(high == 0) begin 
+        out = 4'b0000;
+        rst = 0;
+    end
+    else begin
+        rst = 1;
+        case(cnt)
+            3'b000:begin
+                temp = in1;
+                temp_o[0] = y;
+                end
+            3'b001:begin
+                temp = in2;
+                temp_o[1] = y;
+                end
+            3'b010:begin
+                temp = in3;
+                temp_o[2] = y;
+                end
+            3'b011:begin
+                temp = in4;
+                temp_o[3] = y;
+                out = temp_o;
+                rst = 0;
+                end
+        endcase
+        
+    end
 end
 endmodule
--- twobit_26x18_mesh.v
+++ twobit_26x18_mesh.v
@@ -1,25 +1,22 @@
 `timescale 1ns / 1ps
 module twobit_26x18_mesh(
-input [51:0] inp,      // 26 inputs, 2 bit each, concatenated
-input [4:0] row,       // for inputting the values at the right place
+input [935:0] inp,     
 input clk,
-input high, //when high = 1, all the inputs have been scaned. Output will then be generated after 4 clock cycles.
-output [467:0] out
+input high,
+output [1871:0] out
 );
 
 integer t;
-//Input section
 reg [1:0] in[0:467];
-always @ (*) begin
-    for ( t = 0 ; t<26 ; t=t+1 ) begin
-	    in[26*row+t] = {inp[2*t],inp[(2*t)+1]};
+always @ (posedge clk) begin
+    for ( t = 0 ; t<468 ; t=t+1 ) begin
+	    in[467-t] = {inp[(2*t)+1],inp[2*t]};
     end
 end
-
 genvar j;
 generate
 	for ( j = 0 ; j<468 ; j=j+1 ) begin: comparator_block
-		node wirexy (in[j],in[(j-26+468)%468],in[(j-j%26)+(j-1+26)%26],in[(j-j%26)+(j+1+26)%26],in[(j+26+468)%468],clk,high,out[467-j]);
+		node wirexy (in[j],in[(j-26+468)%468],in[(j-j%26)+(j-1+26)%26],in[(j-j%26)+(j+1+26)%26],in[(j+26+468)%468],clk,high,{out[4*(467-j)+3],out[4*(467-j)+2],out[4*(467-j)+1],out[4*(467-j)]});
 	end
 endgenerate
 
